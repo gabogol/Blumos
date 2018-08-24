78436764821c30434e028443093579ed4b48e63b
Gabriel Toro <gabriel.toro@gmail.com>
2018-08-24 20:52:23 -0300

    pagina valores wp

--- pagina-valores.php
+++ pagina-valores.php
@@ -1,15 +1,15 @@
-<?php /* Template Name: Pagina Nosotros */ ?>
+<?php /* Template Name: Pagina Valores */ ?>
 
 <<?php get_header();?>
 <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-<body style="background-image: url('http://www.myapp.cl/clientes/blumos/wp-content/uploads/2018/08/bg-nosotros.jpg');background-position: center top;background-repeat: no-repeat;">
+<body style="background-image: url('http://www.myapp.cl/clientes/blumos/wp-content/themes/Blumos-2018/img/bg-valores.jpg');background-position: center top;background-repeat: no-repeat;">
   
   <?php get_header('menu');?>
   
   <div class="wrapper">
       
       <!-- Page Content -->
-      <div id="content" class="pagina-nosotros">
+      <div id="content" class="pagina-valores">
           <!--Nav-->
           <nav class="navbar navbar-expand-lg int">
               <div class="container-fluid">
@@ -26,232 +26,90 @@
                 </a>
               </div>
           </nav>
-
-
-          <!--Contenido Page-->
-           <div class="container p-5">
+          <!--Cabecera Page-->
+            <div class="container">
                 <div class="row">
-                    <div class="col-md-6">
-                        <h6 class="pb-3"><?php the_title();?></h6>
-                        <p class="display-5 pt-2"><?php the_field( "bajada_principal" ); ?></p>
-                    </div>
-                    <div class="col-md-6">
-                        <img src="<?php bloginfo( 'template_url' ); ?>/img/curva-page.svg" alt="" class="curva-texto img-fluid">
-                        <p><?php the_field( "txt_principal" ); ?></p>
+                    <div class="col-md-12 text-center">
+                        <h2 class="pt-2 display-2"><?php the_title();?></h2>
                     </div>
                 </div>
             </div>
 
-             <!--Nosotros-->
-             <article id="secNosotros">
-                <div class="container">
-                    <div class="row">
-                        <div class="h720 d-flex align-items-center">
-                            <div class="col-md-1">
-                                <img src="<?php bloginfo( 'template_url' ); ?>/img/col-izq-img.png" alt="img-fluid">
-                            </div>
-                            <div class="col-md-10">
-                                <p class="lead"><?php the_field( "txt_nosotros" ); ?></p>
-                            </div>
-                            <div class="col-md-1">
-                                <img src="<?php bloginfo( 'template_url' ); ?>/img/col-der-img.png" alt="img-fluid">
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </article>
+         <!--Contenido Page-->
 
-            <!--Historia-->
-            <article id="secHistoria">
-                <div class="container py-5 h720 d-flex align-items-center">
-                    <div class="row ">
-                        <div class="col-md-5 align-self-start">
-                            <h2 class="text-center"><?php the_field( "titulo_historia" ); ?></h2>
-                        </div>
-                        <div class="col-md-1">
-                            <img src="<?php bloginfo( 'template_url' ); ?>/img/curva-horizontal.png" alt="" class="img-fluid">
-                        </div>
-                        <div class="col-md-6">
-                            <p class="pt-4"><?php the_field( "txt_historia" ); ?></p>
-                        </div>
-                    </div>
-                </div>
-            </article>
-
-            <!--Grupo-->
-            <article id="secGrupo">
-                <div class="container">
-                    <div class="row">
-                        <div class="col-md-6 pt-5 mt-5">
-                            <h2 class="pb-5"><?php the_field( "titulo_grupo" ); ?></h2>
-                            <p class="pl-md-5 p-3"><?php the_field( "txt_grupo" ); ?></p>
-                        </div>
-                        <div class="col-md-6 col-grupo">
-                            <img src="<?php the_field( "img_grupo" ); ?>" alt="" class="img-fluid">
-                        </div>
-                    </div>
-                </div>
+            <!---Calidad-->
+            <article id="secCalidad">
                 <div class="container mt-5">
-                    <div class="row">
-                        <div class="col-md-6">
-                            <h6 class="pb-3"><?php the_field( "titulo_empresas" ); ?></h6>
-                        </div>
-                        <div class="col-md-6">
-                            <img src="<?php bloginfo( 'template_url' ); ?>/img/curva-page.svg" alt="" class="curva-texto img-fluid">
-                        </div>
-                    </div>
-                </div>
-
-                 <!--Empresas-->
-                 <div class="container mt-3">
-                    <div class="row no-gutters">
-                        <div class="col-md-4">
-                            <div class="cuadrito">
-                                <img src="<?php the_field( "imagen_empresa_1" ); ?>" alt="Blumos" class="img-cuadrito">
-                                <div class="fondo">
-                                    <div class="textito w-75">
-                                        <h6><?php the_field( "titulo_empresa_1" ); ?></h6>
-                                        <p><?php the_field( "texto_empresa_1" ); ?></p>
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="cuadrito">
-                                <img src="<?php the_field( "imagen_empresa_2" ); ?>" alt="Blumos" class="img-cuadrito">
-                                <div class="fondo">
-                                    <div class="textito w-75">
-                                        <h6><?php the_field( "titulo_empresa_2" ); ?></h6>
-                                        <p><?php the_field( "texto_empresa_2" ); ?></p>
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="cuadrito">
-                                <img src="<?php the_field( "imagen_empresa_3" ); ?>" alt="Blumos" class="img-cuadrito">
-                                <div class="fondo">
-                                    <div class="textito w-75">
-                                        <h6><?php the_field( "titulo_empresa_3" ); ?></h6>
-                                        <p><?php the_field( "texto_empresa_3" ); ?></p>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="col-md-4 mt-5 pt-3">
-                        <div class="cuadrito">
-                                <img src="<?php the_field( "imagen_empresa_4" ); ?>" alt="Blumos" class="img-cuadrito">
-                                <div class="fondo2">
-                                    <div class="textito w-75">
-                                        <h6><?php the_field( "titulo_empresa_4" ); ?></h6>
-                                        <p><?php the_field( "texto_empresa_4" ); ?></p>
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="cuadrito">
-                                <img src="<?php the_field( "imagen_empresa_5" ); ?>" alt="Blumos" class="img-cuadrito">
-                                <div class="fondo2">
-                                    <div class="textito w-75">
-                                        <h6><?php the_field( "titulo_empresa_5" ); ?></h6>
-                                        <p><?php the_field( "texto_empresa_5" ); ?></p>
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="cuadrito">
-                                <img src="<?php the_field( "imagen_empresa_6" ); ?>" alt="Blumos" class="img-cuadrito">
-                                <div class="fondo2">
-                                    <div class="textito w-75">
-                                        <h6><?php the_field( "titulo_empresa_6" ); ?></h6>
-                                        <p><?php the_field( "texto_empresa_6" ); ?></p>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="col-md-4 mt-5 pt-5">
-                        <div class="cuadrito">
-                                <img src="<?php the_field( "imagen_empresa_7" ); ?>" alt="Blumos" class="img-cuadrito">
-                                <div class="fondo">
-                                    <div class="textito w-75">
-                                        <h6><?php the_field( "titulo_empresa_7" ); ?></h6>
-                                        <p><?php the_field( "texto_empresa_7" ); ?></p>
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="cuadrito">
-                                <img src="<?php the_field( "imagen_empresa_8" ); ?>" alt="Blumos" class="img-cuadrito">
-                                <div class="fondo">
-                                    <div class="textito w-75">
-                                        <h6><?php the_field( "titulo_empresa_8" ); ?></h6>
-                                        <p><?php the_field( "texto_empresa_8" ); ?></p>
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="cuadrito">
-                                <img src="<?php the_field( "imagen_empresa_9" ); ?>" alt="Blumos" class="img-cuadrito">
-                                <div class="fondo">
-                                    <div class="textito w-75">
-                                        <h6><?php the_field( "titulo_empresa_9" ); ?></h6>
-                                        <p><?php the_field( "texto_empresa_9" ); ?></p>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </article>
-
-             <!--Internacional-->
-             <article id="secInter">
-                <div class="container">
-                    <div class="row">
-                        <div class="col-md-12">
-                            <h2><?php the_field( "titulo_internacional" ); ?></h2>
-                        </div>
-                    </div>
-                    <div class="row der-iner">
-                        <div class="col-md-6"></div>
-                        <div class="col-md-5">
-                            <img src="<?php the_field( "icono_plantas" ); ?>" alt="" class="float-left pr-1"> 
-                            <h5 class="pt-2"><?php the_field( "titulo_plantas" ); ?></h5>
-                            <img src="<?php the_field( "icono_oficinas" ); ?>" alt="" class="float-left pr-1">
-                            <h5 class="pt-2"><?php the_field( "titulo_oficinas" ); ?></h5>
-                        </div>
-                    </div>
-                    <div class="row">
-                        <div class="col-md-3"></div>
-                        <div class="col-md-5">
-                            <h5 class="pb-4"><?php the_field( "bajada_internacional" ); ?></h5>
-                        </div>
-                    </div>
                     <div class="row pt-5">
-                        <div class="col-md-3"></div>
-                        <div class="col-md-7">
-                            <p><?php the_field( "txt_internacional" ); ?></p>
+                        <div class="col-md-4 offset-md-1">
+                            <img src="<?php the_field( "img_calidad" ); ?>" alt="" class="img-fluid">
                         </div>
-                    </div>
-                    <div class="row mt-5 pt-5">
-                        <div class="col-md-3">
-                            <h5 class="pb-4"><?php the_field( "pais_1" ); ?></h5>
-                            <p><strong><?php the_field( "pais_1_sub" ); ?></strong></p>
-                            <p>
-                                <?php the_field( "pais_1_txt" ); ?>
-                            </p>
-                            <p>
-                                Tel: <strong><?php the_field( "pais_1_tel" ); ?></strong>
-                                <a href="<?php the_field( "pais_1_link" ); ?>" target="_blank"><?php the_field( "pais_1_link_txt" ); ?></a>
-                            </p>
-                        </div>
-                        <div class="col-md-1"></div>
-                        <div class="col-md-3">
-                            <h5 class="pb-4"><?php the_field( "pais_2" ); ?></h5>
-                            <p><strong><?php the_field( "pais_2_sub" ); ?></strong></p>
-                            <p>
-                                <?php the_field( "pais_2_txt" ); ?>
-                            </p>
-                            <p>
-                                Tel: <strong><?php the_field( "pais_2_tel" ); ?></strong>
-                                <a href="<?php the_field( "pais_2_link" ); ?>" target="_blank"><?php the_field( "pais_1_link_txt" ); ?></a>
-                            </p>
+                        <div class="col-md-7 text-center px-5">
+                            <h3 class="pb-4"><?php the_field( "titulo_calidad" ); ?></h3>
+                            <p class="lead"><?php the_field( "txt_calidad" ); ?></p>
+                            <i class="fas fa-quote-right fa-5x float-right" style="margin:-35px 55px 0 0;color:#214295;opacity:0.3;"></i>
                         </div>
                     </div>
                 </div>
             </article>
+
+            <!---Trabajo-->
+            <article id="secTrabajo">
+                <div class="container">
+                    <div class="row pt-5">
+                        <div class="col-md-7 text-center px-5">
+                            <h3 class="pb-4"><?php the_field( "titulo_trabajo" ); ?></h3>
+                            <p class="lead"><?php the_field( "txt_trabajo" ); ?></p>
+                            <i class="fas fa-quote-right fa-5x float-right" style="margin:-35px 55px 0 0;color:#214295;opacity:0.3;"></i>
+                        </div>
+                        <div class="col-md-4 offset-md-1"><img src="<?php the_field( "img_trabajo" ); ?>" alt="" class="img-fluid"></div>
+                    </div>
+                </div>
+            </article>
+
+            <!---Eficiencia-->
+            <article id="secEficiencia">
+                <div class="container mt-5">
+                    <div class="row pt-5">
+                        <div class="col-md-4 offset-md-1"><img src="<?php the_field( "img_eficiencia" ); ?>" alt="" class="img-fluid"></div>
+                        <div class="col-md-7 text-center px-5">
+                            <h3 class="pb-4"><?php the_field( "titulo_eficiencia" ); ?></h3>
+                            <p class="lead"><?php the_field( "txt_eficiencia" ); ?></p>
+                            <i class="fas fa-quote-right fa-5x float-right" style="margin:-35px 55px 0 0;color:#214295;opacity:0.3;"></i>
+                        </div>
+                    </div>
+                </div>
+            </article>
+
+             <!---Confianza-->
+             <article id="secConfianza">
+                <div class="container">
+                    <div class="row pt-5">
+                        <div class="col-md-7 text-center px-5">
+                            <h3 class="pb-4"><?php the_field( "titulo_confianza" ); ?></h3>
+                            <p class="lead"><?php the_field( "txt_confianza" ); ?></p>
+                            <i class="fas fa-quote-right fa-5x float-right" style="margin:-35px 55px 0 0;color:#214295;opacity:0.3;"></i>
+                        </div>
+                        <div class="col-md-4 offset-md-1"><img src="<?php the_field( "img_confianza" ); ?>" alt="" class="img-fluid"></div>
+                    </div>
+                </div>
+            </article>
+
+            <!---Responsabilidad-->
+            <article id="secResponsabilidad">
+                    <div class="container mt-5">
+                        <div class="row pt-5">
+                            <div class="col-md-4 offset-md-1">
+                                <img src="<?php the_field( "img_resp" ); ?>" alt="" class="img-fluid">
+                            </div>
+                            <div class="col-md-7 text-center px-5">
+                                <h3 class="pb-4"><?php the_field( "titulo_resp" ); ?></h3>
+                                <p class="lead"><?php the_field( "txt_resp" ); ?></p>
+                                <i class="fas fa-quote-right fa-5x float-right" style="margin:-35px 55px 0 0;color:#214295;opacity:0.3;"></i>
+                            </div>
+                        </div>
+                    </div>
+                </article>
           
           <?php endwhile; endif;?>
       </div>
